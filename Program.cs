2da1661fffda9fcffd42a65aef54c8ff9545fdbf
Tim Dunham <tim.dunham@infor.com>
2019-11-08 14:52:41 -0500

    initial run of Wood Door

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using Infor.CPQ.ConfiguratorLoadTestFixtures;
+using ReservationPerformanceTests.ReservationPerformanceTests;
+using Zoxive.HttpLoadTesting.Client;
+using Zoxive.HttpLoadTesting.Framework.Core;
+using Zoxive.HttpLoadTesting.Framework.Core.Schedules;
+using Zoxive.HttpLoadTesting.Framework.Model;
+
+namespace ReservationPerformanceTests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Specify schedules. Add a few users run for a while and remove them. You can run any schedule in any order.
+            // As long as you have active users!
+            var schedule = new List<ISchedule>
+            {    
+                // Add Users over a period of time
+                new AddUsers(totalUsers: 10, usersEvery: 1, seconds: 1),
+                //new AddUsers(totalUsers: 30, usersEvery: 1, seconds: 15),
+                // Run for a duration of time
+                new Duration(15m),
+                // Remove Users over a period of time
+                new RemoveUsers(usersToRemove: 10, usersEvery:1, seconds: 1)
+            };
+
+            var tests = new List<ILoadTest>
+            {
+                new RunWoodDoor("CPQUSAX3_AX3")
+            };
+
+            var users = new List<IHttpUser>
+            {
+                UserFactory.CreateUser("https://cfgax3.cpq.awsdev.infor.com/", "key here", "secret here", "CPQUSAX3_AX3", tests),
+            };
+
+            var testExecution = new LoadTestExecution(users);
+            
+            WebClient.Run(testExecution, schedule, null, args);
+        }  
+    }
+}
---
+++ Tests/RunWoodDoor.cs
@@ -0,0 +1,51 @@
+using System;
+using System.Threading.Tasks;
+using Infor.CPQ.ConfiguratorLoadTestFixtures;
+using Zoxive.HttpLoadTesting.Framework.Core;
+using Zoxive.HttpLoadTesting.Framework.Http;
+
+namespace ReservationPerformanceTests.ReservationPerformanceTests
+{
+    internal class RunWoodDoor : ILoadTest
+    {
+        public string _tenant { get; }
+        public RunWoodDoor(string tenant)
+        {
+            _tenant = tenant;
+        }
+        public string Name => nameof(RunWoodDoor);
+
+        public async Task Execute(IUserLoadTestHttpClient loadLoadTestHttpClient)
+        {
+            var test = new ConfigurationV1(loadLoadTestHttpClient, _tenant, "AMC", "Wood Door v1")
+                .WithIntegrationParameter("CurrencyCode", "", "string")
+                .WithIntegrationParameter("ExchangeRate", "1", "number");
+
+            try
+            {
+                await test.StartAsync();
+                await test.ConfigureWithRandomOptionAsync("Select your wood species:", new string[] { }, "Species");
+                await test.ConfigureWithRandomOptionAsync("Stain Color:", new string[] { }, "Stain");
+                await test.Continue();
+                await test.ConfigureAsync("Door Style:", "AA", "Set Style");
+                await test.ConfigureAsync("Width:", "28", "Width");
+                await test.ConfigureAsync("Height:", "84", "Height");
+                await test.ConfigureAsync("Hinges:", "True", "Hinges");
+                await test.Finalize();
+                Console.WriteLine("Success");
+            }
+            catch (System.Exception e)
+            {
+                Console.WriteLine($"FAILED {e.Message}");
+                await test.Cancel();
+                //throw e; 
+            }
+
+        }
+
+        public Task Initialize(ILoadTestHttpClient loadLoadTestHttpClient)
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
